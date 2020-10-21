0b4556a923d7d8898a7af9aa4842b3390ee9510b
Saranyaram4291 <70197800+Saranyaram4291@users.noreply.github.com>
2020-10-21 10:17:50 +0530

    Added new file

---
+++ AppTest.java
@@ -0,0 +1,12 @@
+package SDET_Cucumber_Activity.SDET_Cucumber_Activity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
