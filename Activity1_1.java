4c1bfec50044062b6e53802a5b5bfe825d2d59ed
prasanthi-sdet <70203859+prasanthi-sdet@users.noreply.github.com>
2020-10-21 10:21:21 +0530

    Add files via upload
    Added new Java File

--- Activity1_1.java
+++ Activity1_1.java
@@ -1,21 +1,20 @@
-package session1;
+package javaActivity1;
 
 public class Activity1_1 {
 
-    public static void main(String[] args) {
-	    // Object initialization
-	    Car toyota = new Car();
-		// Add default values
-	    toyota.make = 2015;
-	    toyota.color = "White";
-	    toyota.transmission = "Manual";
-	    toyota.modelNumber = "qpri134";
-	    toyota.model = "Swift";
-	
-		//Using Car class method
-		toyota.displayCharacterstics();
-		toyota.accelerate();
-		toyota.brake();
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car carName=new Car();
+		
+		carName.make=2014;
+		carName.color="Black";
+		carName.transmission="Manual";
+		
+		carName.displayCharacteristics();
+		carName.accelarate();
+		carName.brake();
+
+	}
 
 }
